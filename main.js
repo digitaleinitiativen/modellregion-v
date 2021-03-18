472bbf3ccd5065b2c57e6a456f048bfc0f7064f2
joemoe <johannes@crate.io>
2021-03-18 18:46:50 +0100

    and yeet he walks

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 class GameScene extends Phaser.Scene {
 	constructor() {
 		super('game');
+
+		this.markus = null;
 	}
 
 	preload() {
+		this.load.spritesheet('markus-w', 'img/markus-w.png', {
+			frameWidth: 96,
+			frameHeight: 128
+		});
+		this.load.image('background', 'img/background.png');
+		this.load.image('floor', 'img/floor.png');
 	}
 
 	create() {
+		this.add.image(512, 128, 'background');
+		this.add.image(512, 256 - 32 / 2, 'floor');
+
+		this.markus = this.add.sprite(128, 256 - 34 - 128 / 2, 'markus-w', 0);
+		this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('markus-w', { frames: [ 0, 1, 2, 3, 4, 5, 6, 7 ] }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.markus.play('walk');
 	}
 
 	update() {
